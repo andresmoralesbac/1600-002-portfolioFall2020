b3c0ad72ca9ff42f3e15124644de11a4ad5e76d9
Andres Morales <andres.d.morales@gmail.com>
2020-11-19 16:19:22 -0700

    pop dom w/ correct img created button new & show

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,22 +1,99 @@
-const pokeGrid = document.querySelector('.pokemonGrid')
 
-async function loadData() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
-    const data = await response.json()
-    populatePokePage(data)
-}
-
-function populatePokePage(data) {
-    const allPokemon = data.results
-    for (const pokemon of allPokemon) {
-        let pokeCard = document.createElement('div')
-        pokeCard.className = 'pokeCard'
-        let pokeName = document.createElement('h3')
-        pokeName.textContent = pokemon.name
-        
-        pokeCard.appendChild(pokeName)
-        pokeGrid.appendChild(pokeCard)
+//reusable async function to fetch data from the provided url
+async function getApiData(url) {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
     }
 }
+// now use ther async getAPIDatafunction 
+function loadPage() {
+    getApiData(`https://pokeapi.co/api/v2/pokemon`).then //?limit=25&offset=149`).then
+        (async (data) => {
+            for (const pokemon of data.results) {
+                await getApiData(pokemon.url).then((pokeData) => {
+                    populatePokeCard(pokeData)
+                })
+            }
+        })
+}
 
-loadData()
+const pokeGrid = document.querySelector('.pokemonGrid')
+const loadButton = document.querySelector('.load')
+const newPokemomButton = document.querySelector('.newPokemon')
+
+newPokemonButton.addEventListener('click', () => {
+    let pokeName = prompt('What is your new Pokemon name?')
+    let newPokemon = new Pokemon(pokeName, 400, 200, ['Smoke out', 'Perish Song'] )
+})
+
+loadButton.addEventListener('click', () =>{
+    loadPage()
+    loadButton.disabled = true
+})
+
+function populatePokeCard(singlePokemon) {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', function () {
+        pokeCard.classList.toggle('is-flipped');
+    })
+
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeCard.appendChild(populateCardBack(singlePokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card__face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImage = document.createElement('img')
+    frontImage.src = `../images/pokemon/${getImageFileName(pokemon)}.png`
+    pokeFront.appendChild(frontImage)
+    pokeFront.appendChild(frontLabel)
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card__face card__face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = `${pokemon.moves.length}moves`
+    backLabel.addEventListener('click', () => getMovesDetails(pokemon.moves))
+    pokeBack.appendChild(backLabel)
+    return pokeBack
+}
+
+function getMovesDetails (pokemonMoves) {
+    const movesURL = pokemonMoves[0].move.url
+    return getApiData(movesURL).then((data) => data.type.name)
+}
+
+
+function getImageFileName(pokemon) {
+    if (pokemon.id < 10) {
+        return `00${pokemon.id}`
+    } else if (pokemon.id > 9 && pokemon.id < 100) {
+        return `0${pokemon.id}`
+    } else if (pokemon.id > 99 && pokemon.id < 810) {
+        return `${pokemon.id}`
+    }
+    return `pokeball`
+}
+
+function Pokemon(name, height, weight, abilities) {
+    this.name = name
+    this.height = height
+    this.weight = weight
+    this.abilities = abilities
+    this.id = 900
+}
+
